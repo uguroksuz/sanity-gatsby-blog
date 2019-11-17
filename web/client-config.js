76620c81cb9416483755d19104f3c3e498877881
Tora Toraman <toratoraman@gmail.com>
2019-11-17 16:16:01 +0300

    Update client-config.js

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || '0ze880dc',
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'wzscd3fa',
     dataset: process.env.GATSBY_SANITY_DATASET || 'production'
   }
 }
